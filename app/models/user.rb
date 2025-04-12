ebaf4259bd1c33858b1a89047b51f2bf3a48f8a0
Edmilson Barreto <edsb1981@gmail.com>
2025-04-12 02:37:51 -0300

    feat: adiciona associações has_many ao modelo User. Fixes #17

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise já tá aqui por padrão
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :service_types, dependent: :destroy
+  has_many :availabilities, dependent: :destroy
+  has_many :user_genres, dependent: :destroy
+  has_many :music_genres, through: :user_genres
 end
