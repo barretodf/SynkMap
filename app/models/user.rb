2b88e7a012dd3641b93348020e7fd665d54c0ee1
Edmilson Barreto <edsb1981@gmail.com>
2025-04-21 15:35:56 -0300

    Feat: criado espaço para foto de perfil com botões de atualização #32

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,3 +12,19 @@ class User < ApplicationRecord
     self.genres = names.split(",").map(&:strip).uniq.map { |name| genres.build(name: name) }
   end
 end
+
+class User < ApplicationRecord
+  validates :name, presence: true
+  has_many :genres, dependent: :destroy
+  has_many :prices, dependent: :destroy
+  has_one_attached :banner
+  has_one_attached :profile_picture
+
+  def genre_names
+    genres.pluck(:name).join(", ")
+  end
+
+  def genre_names=(names)
+    self.genres = names.split(",").map(&:strip).uniq.map { |name| genres.build(name: name) }
+  end
+end
